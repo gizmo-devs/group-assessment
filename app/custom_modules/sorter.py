cf6ebd04bf941ba3dd3ce4db3145ae1ee5a0537f
bradcruick <bradcruick@hotmail.co.uk>
2021-03-08 16:02:45 +0000

    rename variables and use return instead of print.

--- app/custom_modules/sorter.py
+++ app/custom_modules/sorter.py
@@ -9,13 +9,13 @@ def sorter(data, sort_option):
   # sort data depending on option chosen
   if sort_option == 'date':
     sorted_date = sorted(data, key=lambda x: datetime.strptime(x['Month'], '%Y-%m'))
-    print(sorted_date)
+    return sorted_date
   if sort_option == 'distance':
-    sorted_date = sorted(data, key=lambda x: x['Distance'], reverse=False)
-    print(sorted_date)
+    sorted_distance = sorted(data, key=lambda x: x['Distance'], reverse=False)
+    return sorted_distance
   if sort_option == 'category':
-    sorted_date = sorted(data, key=lambda x: x['Crime type'], reverse=False)
-    print(sorted_date)
+    sorted_category = sorted(data, key=lambda x: x['Crime type'], reverse=False)
+    return sorted_category
 
 sorter(data, 'date')
 
