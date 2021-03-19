d4c9a1d0ba65c9c73b3f182ff145bf8ed19275dc
Craig Attwood <ca487@exeter.ac.uk>
2021-03-19 15:10:29 +0000

    apply code changes to get users input for export filename

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from os import path
 from .custom_modules import file_handler as fh
 from .custom_modules import postcode_validate as pv
 from .custom_modules import radius_validate as rv
-from .custom_modules import sort_option as so
+from .custom_modules import user_interaction as ui
 from .custom_modules import sorter as custom_sort
 from .custom_modules import fileoutputer as output
 from .custom_modules import filter as custom_filter
@@ -18,23 +18,12 @@ def init():
     print("Loading postcodes...")
     postcode_data = fh.load_file(path.join("postcodes", "postcodes.csv"))
 
-    # How to use/display the data.
-    """
-    print("Showing 1st element of crime data:")
-    print(crime_data[0])
-    print("Showing 1st element of Postcode Data:")
-    print(postcode_data[0])
-    """
-
     # Validate postcode
     pcode = pv.user_pcode()
 
     # Get 1st element that matches.
     pcode_coord = [(float(x['ETRS89GD-Lat']), float(x['ETRS89GD-Long'])) for x in postcode_data \
                    if x['Postcode'].replace(" ", "").strip() == pcode.upper()][0]
-
-    # Uncomment to show Postcode.
-    # print(pcode_coord)
 
     # Validate Radius
     r = rv.user_radius()
@@ -48,8 +37,8 @@ def init():
     sort_by = ""
 
     # Sort the results
-    if so.ask_to_sort():
-        sort_by = so.sort_option()
+    if ui.ask_to_sort():
+        sort_by = ui.sort_option()
 
     if sort_by in ['date', 'distance', 'category']:
         crime_data = custom_sort.sorter(crime_data, sort_by)
@@ -58,9 +47,8 @@ def init():
         print("No results found. Nothing to export. Ending")
         exit(0)
 
-    # ask for export name
-    # export_name = so.export_name()
-    export_name = "exmaple_sjdghvsh"
+    # Get user input for export name
+    export_name = ui.export_name()
 
     # export to csv
     output.file_outputer(export_name, crime_data)
