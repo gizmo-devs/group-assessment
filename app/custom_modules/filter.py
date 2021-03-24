9b934084726a68850872dbe7fa2877541101b312
BenjaminMain <bm452@exeter.ac.uk>
2021-03-24 12:50:10 +0000

    added tenuous tests

--- app/custom_modules/filter.py
+++ app/custom_modules/filter.py
@@ -1,8 +1,7 @@
 """
 Filtering the dictionary by radius.
 """
-import math
-from ..assigned_modules import geodist as gd
+from app.assigned_modules import geodist as gd
 
 
 def Filter(crime_data, postcode_latlng, radius):
@@ -20,3 +19,26 @@ def Filter(crime_data, postcode_latlng, radius):
                 filtered_data.append(i)
 
     return filtered_data
+
+if __name__ == '__main__':
+    #using the file handler for loading the crime files
+    import file_handler as fh
+    
+    crime_data = fh.load_files_in_directory("crime_files")
+    print("Initial Data length")
+    print(len(crime_data))
+    
+    print("\nFiltered Data Length from point 1")
+    filtered_data_1 = Filter((crime_data), (50.71422888, -2.4366918), 5)
+    print(len(filtered_data_1))
+    assert(len(crime_data)>len(filtered_data_1))
+    
+    print("\nFiltered Data Length from point 2")
+    filtered_data_2 = Filter((crime_data), (50.71799154, -3.52638455), 5)
+    print(len(filtered_data_2))
+    assert(len(crime_data)>len(filtered_data_2))
+    
+    print("\nFiltered Data Length from point 3")
+    filtered_data_3 = Filter((crime_data), (50.50699801, -4.77120732), 5)
+    print(len(filtered_data_3))
+    assert(len(crime_data)>len(filtered_data_3))
